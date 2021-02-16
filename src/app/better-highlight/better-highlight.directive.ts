6c8875e90866374e1b42d47618e828b77b7b9237
AnwaratneMANA <it19086408@my.sliit.lk>
2021-02-16 16:15:15 +0530

    @HostBinding example with inserting color from outside (Via HTML) using @Input annotation.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,26 +1,30 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @Input() defaultColor = 'transparent';
+  @Input() highlighColor = 'blue';
+
+  @HostBinding('style.backgroundColor') backgroundColor;
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event){
     // @ts-ignore
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', false, false);
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlighColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event){
     // @ts-ignore
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent', false);
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 
