7905b7e10938468457c46d942ca5c48f582bda10
AnwaratneMANA <it19086408@my.sliit.lk>
2021-02-16 15:42:04 +0530

    How to change style of elements dynamically when mouse entered and mouse leave the element @HostListener

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,23 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
-
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    // @ts-ignore
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', false, false);
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    // @ts-ignore
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent', false);
   }
 
 
