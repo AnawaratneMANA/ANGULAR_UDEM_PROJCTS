f91cb6a23c9774d34c88488798414eb8177431c7
Nirmith Akash <IT19086408@my.sliit.lk>
2021-02-18 11:38:21 +0530

    Services change to the type where it will have only one instance (removed the service from the providers)

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {LoggingService} from '../logging.service';
+import {AccountService} from '../account.service';
 
 @Component({
   selector: 'app-account',
@@ -8,16 +9,17 @@ import {LoggingService} from '../logging.service';
   providers: [LoggingService]
 })
 export class AccountComponent {
-  @Input() account: {name: string, status: string};
+  @Input() account: { name: string, status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService) {
+  constructor(private loggingService: LoggingService,
+              private accountService: AccountService
+  ) {
 
   }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountService.updateAccount(this.id, status);
     this.loggingService.logStatusChange(status);
   }
 }
