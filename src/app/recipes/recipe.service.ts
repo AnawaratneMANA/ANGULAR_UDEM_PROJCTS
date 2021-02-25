c09e27d0b8ecd76e0cda0095596f0bab073e1418
Nirmith Akash <it19086408@my.sliit.lk>
2021-02-25 15:55:36 +0530

    Click population method fixed using the new method

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,15 @@
+import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  getRecipe() {
+    // return a copy of the array.
+    return this.recipes.slice();
+  }
+}
