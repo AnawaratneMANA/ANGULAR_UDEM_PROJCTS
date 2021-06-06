910cd72b08c72de5fbf79463515efa0b5522c1f6
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-07 02:16:12 +0530

    Add a radio button and set the gender.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = "teacher";
   answer = "";
+  genders = ["male", "female"];
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
