032ce6b02cc17e9e9d5b7db1c142ffa365a58fc7
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-07-09 09:04:35 +0530

    Created a dynamically incrementable Form Input Field with FormArray.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,8 +17,8 @@ export class AppComponent implements OnInit{
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'gender': new FormControl('male')
-
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
 
@@ -27,6 +27,11 @@ export class AppComponent implements OnInit{
   }
 
   onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
 
+  getControls (){
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 }
