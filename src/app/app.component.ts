7613edbb5ded1ffd4d2aaf74eee478e515c25886
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-07-09 09:39:09 +0530

    Created a Asynchronous Email validator to check the occurrence of a predefined email.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,7 @@ export class AppComponent implements OnInit{
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -37,10 +38,25 @@ export class AppComponent implements OnInit{
     return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 
+  //Custom Name Validator.
   forbiddenNames(control : FormControl): {[s: string]: boolean}{
     if (this.forbiddenUserNames.indexOf(control.value) !== -1){
       return {"nameIsForbidden": true};
     }
     return null; // This should be null or omit the return statement.
+  }
+
+  //Custom Asynchronous Validator. (Promises)
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(()=> {
+        if(control.value === "test@test.com"){
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    });
+    return promise;
   }
 }
