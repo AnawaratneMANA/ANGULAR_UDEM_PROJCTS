f3203b0d8a4ef345d7e944a033de25f2f965447e
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-07-14 11:32:56 +0530

    Created Router Indexes, Registered the Indexers in the App Module file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -11,6 +9,22 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "users",
+    component: UserComponent
+  },
+  {
+    path: "servers",
+    component: ServerComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +39,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
